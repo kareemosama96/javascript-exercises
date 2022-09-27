2d6dd9cfa98a5f1fdcaa8213ae625c6f055fd935
Kareem <kareemosama9966@gmail.com>
2022-09-27 16:10:26 +0200

    exercise-3 done

--- 03_reverseString/reverseString.js
+++ 03_reverseString/reverseString.js
@@ -1,5 +1,9 @@
-const reverseString = function() {
-
+const reverseString = function(str) {
+    revStr="";
+    for(i = str.length; i > 0; i--){
+        revStr+=str[i-1];
+    }
+    return revStr;
 };
 
 // Do not edit below this line
