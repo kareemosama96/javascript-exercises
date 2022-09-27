1fb07c1faad61a800bb1709c096deebfde4790ae
Kareem <kareemosama9966@gmail.com>
2022-09-27 17:46:36 +0200

    exercise-4 done

--- 04_removeFromArray/removeFromArray.js
+++ 04_removeFromArray/removeFromArray.js
@@ -1,14 +1,15 @@
 const removeFromArray = function(array,...removeThis) {
-    for ( i = 0; i < removeThis.length; i++){
-        
-        for (let j = 0; j < array.length; j++) {
-	        if (array[j] === removeThis[i]){
-	            array.splice(j,1);
-	        }
-        }
+    for (i = 0; i < removeThis.length; i++){
+        (check(array,removeThis[i]))
     }
     return array;
 };
+function check(array,itemToRemove){
+	for(let i = 0; i < array.length; i++){
+    (array[i] === itemToRemove)? array.splice(i,1):array;
+  }  
+}
+
 
 // Do not edit below this line
 module.exports = removeFromArray;
