03c87c6642d1dfc72b1a36e054949a49e4cbaf69
Kareem <kareemosama9966@gmail.com>
2022-09-28 07:58:57 +0200

    exercise-7 done

--- 07_tempConversion/tempConversion.js
+++ 07_tempConversion/tempConversion.js
@@ -1,10 +1,30 @@
-const ftoc = function() {
-
+const ftoc = function(f) {
+  if (!checkNumber(f)){
+    print("Error, enter number");
+  }else{
+    return Math.round((((f-32) * (5/9)) * 10))/10;
+  }
 };
 
-const ctof = function() {
-
+const ctof = function(c) {
+  if (!checkNumber(c)){
+    print("Error, enter number");
+  }else{
+    return Math.round((((c * (9/5)) + 32) * 10))/10;
+  }
 };
+
+function checkNumber(number){
+  if (number == null ||
+     	number == undefined ||
+     	isNaN(number) ||
+     	!isFinite(number)||
+      typeof(number) != "number"){
+    return false;
+  }else{
+    return true;
+  }
+}
 
 // Do not edit below this line
 module.exports = {
